cee3cb0d536649ce2e8dd5433b9691250d3b0b0b
Pearla Mutombo <pmutombo1986@gmail.com>
2026-08-07 22:07:37 -0400

    added hooks and index.html. app.css, app.jsx, main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import { BrowserRouter } from "react-router";
+
 export default function App() {
-    
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
