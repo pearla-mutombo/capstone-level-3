9a965b8e67d709dc6c1ecfde9f72f0aa122835d2
Pearla Mutombo <pmutombo1986@gmail.com>
2026-08-14 11:49:29 -0400

    added support email on data file and edited config file.

--- src/data/benefitsData.js
+++ src/data/benefitsData.js
@@ -21,7 +21,7 @@ export const benefits = [
     id: 4,
     icon: "💬",
     title: "Customer Support",
-    description: "We're here to help whenever you need us. Contact our customer support at (404) 624-6335 .",
+    description: "We're here to help whenever you need us. Contact our customer support at (404) 624-6335 or support@novusmarket.com .",
   },
 ];
 
--- vite.config.js
+++ vite.config.js
@@ -15,3 +15,9 @@ export default defineConfig({
     },
   },
 });
+
+// Note: I configured Vite with my GitHub Pages base path so my React 
+// application knows where it is deployed. I also configured a development
+//  proxy so API requests from my React frontend can communicate with my 
+// local Express server without having to put the localhost server address 
+// throughout my components.
